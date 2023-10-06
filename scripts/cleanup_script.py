9c285175442ee54f0c34737169c36dc96ed6a191
Storm Tuyls <Storm.Tuyls@student.hogent.be>
2023-10-06 14:45:35 +0200

    cleanup_script update

--- scripts/cleanup_script.py
+++ scripts/cleanup_script.py
@@ -3,15 +3,34 @@ import re
 import pandas as pd
 
 
+def titelChange(data):
+    data.columns = data.columns.str.replace('crm_', '')
+    data.info()
+
+
+def ChangeAllData():
+    account_financiele_data()
+    account_activiteit()
+    afspraak_betreft_account_cleaned()
+    afspraak_betreft_contact_cleaned()
+    afspraak_account_gelinkt_cleaned()
+    cdi_pageviews()
+    visits()
+    contact()
+    persoon()
+    sessie_inschrijving()
+
+
 def account_activiteit():
     data = pd.read_csv('../data/Account activiteitscode.csv', sep=",")
+    titelChange(data)
     data.dropna(inplace=True)
     # Add your code to process 'Account Activiteit' here
 
 
 def account_financiele_data():
     data = pd.read_csv('../data/Account.csv', sep=",")
-    data.head()
+    titelChange(data)
     if 'crm_Account_Hoofd_NaCe_Code' in data.columns:
         data.drop('crm_Account_Hoofd_NaCe_Code', axis=1, inplace=True)
     # Add your code to process 'Account financiele data' here
@@ -21,6 +40,8 @@ def afspraak_betreft_account_cleaned():
     data = pd.read_csv('../data/Afspraak betreft account_cleaned.csv', sep=",")
     data['crm_Afspraak_BETREFT_ACCOUNT_KeyPhrases'] = data['crm_Afspraak_BETREFT_ACCOUNT_KeyPhrases'].replace(
         '\[NAME\] ,*', '', regex=True)
+    titelChange(data)
+
     # Add your code to process 'Afspraak betreft account cleaned' here
 
 
@@ -28,6 +49,8 @@ def afspraak_betreft_contact_cleaned():
     data = pd.read_csv('../data/Afspraak betreft contact_cleaned.csv', sep=",")
     data['crm_Afspraak_BETREFT_CONTACTFICHE_KeyPhrases'] = data['crm_Afspraak_BETREFT_CONTACTFICHE_KeyPhrases'].replace(
         '\[NAME\] ,*', '', regex=True)
+    titelChange(data)
+
     # Add your code to process 'Afspraak betreft contact cleaned' here
 
 
@@ -35,6 +58,7 @@ def afspraak_account_gelinkt_cleaned():
     data = pd.read_csv('../data/Afspraak_account_gelinkt_cleaned.csv', sep=",")
     data['crm_Afspraak_ACCOUNT_GELINKT_KeyPhrases'] = data['crm_Afspraak_ACCOUNT_GELINKT_KeyPhrases'].replace(
         '\[NAME\] ,*', '', regex=True)
+    titelChange(data)
     # Add your code to process 'Afspraak betreft gelinkt cleaned' here
 
 
@@ -52,11 +76,13 @@ def visits():
     data = pd.read_csv('../data/CDI visits.csv', sep=",")
     if 'crm_CDI_Visit_Campagne_Code' in data.columns:
         data.drop('crm_CDI_Visit_Campagne_Code', axis=1, inplace=True)
+    titelChange(data)
     # Add your code to process 'CDI visits' here
 
 
 def contact():
     data = pd.read_csv('../data/Contact functie.csv', sep=",")
+    titelChange(data)
     data = data.dropna()
 
 
@@ -64,11 +90,13 @@ def persoon():
     data = pd.read_csv('../data/persoon.csv', sep=",")
     data.replace('Nee', 0, inplace=True)
     data.replace('Ja', 1, inplace=True)
+    titelChange(data)
 
 
 def sessie_inschrijving():
     data = pd.read_csv('../data/Sessie inschrijving.csv', sep=",")
     data.dropna(inplace=True)
+    titelChange(data)
 
 
 # Define a dictionary mapping subcommands to functions
@@ -83,6 +111,7 @@ subcommands = {
     'contact': contact,
     'persoon': persoon,
     'sessie_inschrijving': sessie_inschrijving,
+    'all': ChangeAllData
     # Add more subcommands here...
 }
 
