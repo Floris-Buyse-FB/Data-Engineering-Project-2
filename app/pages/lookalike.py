bddc2ee537990e0ca684f2c293c665d74576a996
Max Milan <max.milan@student.hogent.be>
2023-11-15 11:34:31 +0100

    streamlit now working

--- app/pages/lookalike.py
+++ app/pages/lookalike.py
@@ -2,72 +2,38 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-from utils.app_utils_lookalike_functions import clean_new_contact_data, recommend_lookalikes_CONTACT, get_hulp_df
+from utils.app_utils_lookalike_functions import recommend_lookalikes, get_hulp_df
 
 # Streamlit UI
-st.title('Check out the tabs, starting with the data tab')
+st.title('Create lookalikes for a contact')
 
-# Tabs
-data_tab, rec_tab = st.tabs(['Data', 'Lookalike recommendations'])
+# Get the contact ID
+st.write('Write the contact id of the contact you want to find lookalikes for')
+contact_id = st.text_input('Contact ID', value='10446D11-F363-ED11-9561-6045BD895B5A')
 
-# Data tab
+top_n = st.text_input('How many recommendations do you want?', value=10)
+# make sure the input is a number
 
-with data_tab:
-  csv_file = st.file_uploader('Upload a csv file for contact')
-  df_contact_clean = pd.DataFrame()
-  contact_id = None
+try:
+    top_n = int(top_n)
+except:
+    st.write('Please enter a number')
+    top_n = 10
 
-  if csv_file is not None:
+df_hulp = get_hulp_df()
 
-    df_contact = pd.read_csv(csv_file)
+response_list = recommend_lookalikes(df_hulp,contact_id,top_n)
 
-    try:
-      df_contact_clean = clean_new_contact_data(df_contact)
-    except Exception as e:
-      error_type = e.__getattribute__('__class__').__name__
-      if error_type == 'KeyError':
-        st.write(f"{error_type}: {e} not found in the csv file")
-      else:
-        st.write(f"{error_type}: {e}")
-    
-    if df_contact_clean.empty:
-      st.write('Something went wrong, please check the error message above')
-    else:
-      st.dataframe(df_contact_clean)
-    
-    contact_id = st.text_input("Enter a contact ID for recommendation of lookalikes")
+response_df = pd.DataFrame(response_list)
 
-    if contact_id:
-      df_contact_clean[df_contact_clean['contact_contactpersoon_id'] == contact_id]
-      if df_contact_clean.empty:
-        st.write('Contact ID not found')
-      else:
-        st.write('Contact ID found, please go to the lookalike recommendations tab to see the recommendations')
+st.write('The top ' + str(top_n) + ' lookalikes for contact ' + str(contact_id) + ' are: ')
+st.dataframe(response_df)
 
-# Lookalike recommendations tab
-
-with rec_tab:
-  top_n = st.text_input('How many recommendations do you want?', value=10)
-  # make sure the input is a number
-  try:
-      top_n = int(top_n)
-  except:
-      st.write('Please enter a number')
-      top_n = 10
-  
-  df_hulp = get_hulp_df()
-
-  response_list = recommend_lookalikes_CONTACT(df_hulp,df_contact_clean,contact_id,top_n)
-
-  response_df = pd.DataFrame(response_list)
-
-  st.write('The top ' + str(top_n) + ' lookalikes for contact ' + str(contact_id) + ' are: ')
-  st.dataframe(response_df)
-
-  csv = response_df.to_csv(index=False)
-  st.download_button(
-      label='Download the recommendations',
-      data=csv,
-      file_name=f'recommendations_{date.today()}.csv',
-      mime='text/csv'
-  )
+# give the option to download the response
+csv = response_df.to_csv(index=False)
+st.download_button(
+    label='Download the recommendations',
+    data=csv,
+    file_name=f'recommendations_{date.today()}.csv',
+    mime='text/csv'
+)
--- app/utils/app_utils_lookalike_functions.py
+++ app/utils/app_utils_lookalike_functions.py
@@ -3,91 +3,12 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import os
-import re
 
 CSV_FILE = os.path.join(os.getcwd(), '../data_clean/merged_total_no_keyphrases.csv')
-
-#########################
-# CLEAN NEW CONTACT DATA
-#########################
-
-def titelChange(data):
-    for col in data.columns:
-        if col.startswith('crm_') or col.startswith('CDI_'):
-            data.columns = data.columns.str.replace('crm_', '')
-            data.columns = data.columns.str.replace('CDI_', '')
-
-def create_column_names(dataframe, pk='Contact_Contactpersoon'):
-    columns = dataframe.columns
-    columns = [col + '_id' if col == pk else col for col in columns]
-    columns = [re.sub(r'\W+', '', col) for col in columns]
-    columns = [col.lower() for col in columns]
-    dict_columns = dict(zip(dataframe.columns, columns))
-    return dict_columns
-
-def basic_clean(df):
-    data = df.copy()
-    data.drop_duplicates(inplace=True)
-    numeric = data.select_dtypes(include='number').columns
-    non_numeric = data.select_dtypes(exclude='number').columns
-    data[numeric] = data[numeric].fillna(-1)
-    data[non_numeric] = data[non_numeric].fillna('unknown')
-    return data
-
-def clean_new_contact_data(df):
-    preclean_df = df.copy()
-    titelChange(preclean_df)
-    preclean_df.rename(columns=create_column_names(preclean_df), inplace=True)
-    df_contact = basic_clean(preclean_df)
-
-    # Drop kolommen
-    df_contact = df_contact[df_contact['contact_status'] != 'Inactief']
-    df_contact['contact_functietitel'] = df_contact['contact_functietitel'].str.lower().str.replace(r'[^\w\s]', '', regex=True) \
-                                                            .str.replace('  ', ' ').str.strip()
-    df_contact.drop(['contact_status', 'contact_voka_medewerker','contact_account'], axis=1, inplace=True)
-
-    final_df = df_contact.copy()
-    return final_df
 
 ###########################
 # LOOKALIKE RECOMMANDATIONS
 ###########################
-
-# def clean_merged(df):
-#   new_df = df.copy()
-#   new_df = new_df[['contact_contactpersoon_id','contact_functietitel','functie_naam','account_adres','account_onderneming','afspraak_keyphrases','campagne_campagne_id','campagne_type_campagne','campagne_naam','campagne_soort_campagne']]
-#   new_df = new_df.drop_duplicates(subset=['contact_contactpersoon_id','campagne_campagne_id'], keep='first')
-#   return new_df
-
-# def vectorizing(df):
-#   df2 =df
-#   df2['data'] =df[df.columns[1:]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
-
-#   vectorizer = CountVectorizer()
-#   vectorized = vectorizer.fit_transform(df2['data'])
-#   return vectorized
-
-
-# def similarities(df):
-#   similarities = cosine_similarity(df)
-#   return similarities
-
-# def lookalike_matrix(df):
-#   data = clean_merged(df)
-#   vectorized = vectorizing(data)
-#   similarities = similarities(vectorized)
-#   matrix = pd.DataFrame(similarities,columns=df['contact_contactpersoon_id'],index=df['contact_contactpersoon_id']).reset_index()
-#   return matrix
-
-# def recommend_lookalikes(df, input_person_id):
-#   matrix = lookalike_matrix(df)
-#   recommendations = pd.DataFrame(matrix.nlargest(11,input_person_id)['contact_contactpersoon_id'])
-#   recommendations = recommendations[recommendations['contact_contactpersoon_id']!=input_person_id]
-#   return recommendations
-
-############################################
-# RECOMMENDATIONS BASED ON NEW CONTACT DATA
-############################################
 
 def clean_merged(df):
   new_df = df.copy()
@@ -103,22 +24,22 @@ def vectorizing(df):
   vectorized = vectorizer.fit_transform(df2['data'])
   return vectorized
 
-def similarities_CONTACT(df,contact_df):
-  similarities = cosine_similarity(df,contact_df)
-  return similarities
+def lookalike_matrix(df):
+  vectorized = vectorizing(df)
+  print("Vectorized shape:", vectorized.shape)
 
-def lookalike_matrix_CONTACT(df,contact_df):
-  data = clean_merged(df)
-  vectorized_df = vectorizing(data)
-  vectorized_contact = vectorizing(contact_df)
-  similarities = similarities_CONTACT(vectorized_df,vectorized_contact)
-  matrix = pd.DataFrame(similarities,columns=contact_df['contact_contactpersoon_id'],index=df['contact_contactpersoon_id']).reset_index()
+  similarities = cosine_similarity(vectorized)
+  print("Similarities shape:", similarities.shape)
+
+  matrix = pd.DataFrame(similarities,columns=df['contact_contactpersoon_id'],index=df['contact_contactpersoon_id']).reset_index()
+  print("Matrix shape:", matrix.shape)
   return matrix
 
-def recommend_lookalikes_CONTACT(df,contact_df, input_contact_id, top_n=10):
-  matrix = lookalike_matrix_CONTACT(df,contact_df)
-  recommendations = pd.DataFrame(matrix.nlargest(top_n,input_contact_id)['contact_contactpersoon_id'])
-  recommendations = recommendations[recommendations['contact_contactpersoon_id']!=input_contact_id]
+def recommend_lookalikes(df, input_person_id, top_n=10):
+  df = clean_merged(df)
+  matrix = lookalike_matrix(df)
+  recommendations = pd.DataFrame(matrix.nlargest(top_n,input_person_id)['contact_contactpersoon_id'])
+  recommendations = recommendations[recommendations['contact_contactpersoon_id']!=input_person_id]
   return recommendations
 
 ########################
